0dc4c486f45af505c48a923c4110ac5255a6a14e
Tucker J. Pelletier <tpelletier@bluesageusa.com>
2022-06-21 14:26:02 -0400

    Fixing so the data dump can be restored.

--- 0_initial_users.sql
+++ 0_initial_users.sql
@@ -2,3 +2,4 @@ GRANT ALL PRIVILEGES ON *.* TO 'bss_developers'@'%' IDENTIFIED BY 'waoij5oi34eje
 GRANT ALL PRIVILEGES ON *.* TO 'bluesage'@'%' IDENTIFIED BY 'waoij5oi34ejel';
 GRANT ALL PRIVILEGES ON *.* TO 'bssadmin'@'%' IDENTIFIED BY 'waoij5oi34ejel';
 GRANT ALL PRIVILEGES ON *.* TO 'ro_user'@'%' IDENTIFIED BY 'waoij5oi34ejel';
+GRANT ALL PRIVILEGES ON *.* TO 'bluesage_dev'@'%' IDENTIFIED BY 'waoij5oi34ejel';
